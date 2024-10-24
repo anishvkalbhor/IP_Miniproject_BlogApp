7b84cdfa0019278bf73f59d35bedd6eb3fb0fe2e
anishkalbhor123 <anishkalbhor2020@gmail.com>
2024-10-24 22:19:32 +0530

    Display blog list using api

--- app/api/blog/route.js
+++ app/api/blog/route.js
@@ -12,9 +12,16 @@ LoadDB();
 // API Endpoint to get all blogs
 export async function GET(request) {
 
-    const blogs = await BlogModel.find({});
+    const blogId = request.nextUrl.searchParams.get("id");
+    if (blogId) {
+        const blog = await BlogModel.findById(blogId);
+        return NextResponse.json(blog);
+    }
+    else{
+        const blogs = await BlogModel.find({});
+        return NextResponse.json({blogs});
+    }
 
-    return NextResponse.json({blogs});
 }
 
 // API Endpoint for Uploading blogs
