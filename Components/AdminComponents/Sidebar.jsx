8641f59e0fc1fc66b15e727fa771d740bc961b58
anishkalbhor123 <anishkalbhor2020@gmail.com>
2024-10-25 09:59:38 +0530

    added button for admin panel

--- Components/AdminComponents/Sidebar.jsx
+++ Components/AdminComponents/Sidebar.jsx
@@ -25,6 +25,11 @@ const Sidebar = () => {
             <Image src={assets.email_icon} alt="" width={28} />
             <p>Subscriptions</p>
           </Link>
+
+          <Link href='/' className="flex mt-5 items-center border border-black gap-3 font-medium px-3 py-2 bg-white shadow-[-5px_5px_0px_#000000]">
+            <Image src={assets.email_icon} alt="" width={28} />
+            <p>Home</p>
+          </Link>
         </div>
       </div>
     </div>
--- Components/Header.jsx
+++ Components/Header.jsx
@@ -1,6 +1,7 @@
 import { assets } from '@/Assets/assets'
 import axios from 'axios';
 import Image from 'next/image'
+import Link from 'next/link';
 import React, { useState } from 'react'
 import { toast } from 'react-toastify';
 
@@ -26,10 +27,10 @@ const Header = () => {
 
   return (
     <div className='py-5 px-5 md:px-12 lg:px-28'>
-      <div className='flex justify-between items-center'>
+      <Link href='/admin' className='flex justify-between items-center'>
         <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
-        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started <Image src={assets.arrow}/></button>
-      </div>
+        <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Admin Panel<Image src={assets.arrow}/></button>
+      </Link>
       <div className='text-center my-8'>
         <h1 className='text-3xl sm:text-5xl font-medium'>Latest Blogs</h1>
         <p className='mt-10 max-w-[720px] m-auto text-xs sm:text-base'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptate voluptas placeat vel natus consequatur tenetur laboriosam voluptatibus repellat, voluptatem in assumenda explicabo, nihil eveniet rem! Voluptatum soluta aperiam id. Aperiam?</p>
